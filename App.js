2c75c934a34a3066cbc8347f87eabd6baaf2f56a
ahmed <hamzaa667788@gmail.com>
2024-06-05 13:04:06 -0400

    login screen done

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import SplashScreen from './views/SplashScreenView';
+import LoginPage from './views/LoginPage';
 
 export default function App() {
   return (
-    <SplashScreen />
+    // <SplashScreen />
+    <LoginPage />
   );
 }
 
---
+++ views/LoginPage.js
@@ -0,0 +1,72 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, KeyboardAvoidingView, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+export default function LoginPage() {
+  return (
+    <SafeAreaView style={styles.container}>
+        <KeyboardAvoidingView behavior='padding' style={styles.container}>
+            <Text style={styles.header}>Enter your details</Text>
+
+            <Text style={styles.text}>First Name</Text>
+            <TextInput style={styles.input} />
+            <Text style={styles.text}>Last Name</Text>
+            <TextInput style={styles.input} />
+            <Text style={styles.text}>Enter Email</Text>
+            <TextInput style={styles.input} />
+            <Text style={styles.text}>Password</Text>
+            <TextInput style={styles.input} secureTextEntry={true} />
+            
+            <View style={styles.spacer}>
+            </View>
+
+            <TouchableOpacity style={styles.submitBtn}>
+                <Text style={styles.submitBtnText}>Login</Text>
+            </TouchableOpacity>
+           
+
+            <StatusBar style="auto" />
+        </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    
+  },
+  header: {
+    fontSize: '40px',
+    fontWeight: 'bold',
+    margin: 25,
+  },
+  text: {
+    marginLeft: 10
+  },
+  input: {
+    height: 40,
+    margin: 8,
+    padding: 10,
+    backgroundColor: '#F4F7F8',
+    borderRadius: 5,
+  },
+  submitBtn : {
+    backgroundColor: '#00388E',
+    paddingVertical: 10,
+    paddingHorizontal: 150,
+    borderRadius: 10,
+    alignItems: 'center',
+    alignSelf: 'center',
+    marginTop: 20,
+
+  },
+  submitBtnText : {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  spacer: {
+    flex: 1,
+  }
+});
