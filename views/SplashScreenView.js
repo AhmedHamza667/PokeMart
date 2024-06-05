83bc1c685922b39ce8510e8f488a1f7c557e896f
ahmed <hamzaa667788@gmail.com>
2024-06-05 10:47:35 -0400

    Splash screen done

---
+++ views/SplashScreenView.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function SplashScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Fashion Tap</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: '40px',
+  }
+});
